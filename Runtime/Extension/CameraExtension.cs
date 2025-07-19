0bc7a33cf5140778f957d770c89fa7bcb7119d0a
zhulkov <zhulkov@imc-s.ru>
2025-07-19 22:09:17 +0400

    Расширение для определения границ видимости камеры. Расширение для получение предыдущего и сделующего значения enum

---
+++ Runtime/Extension/CameraExtension.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace UnityGameLib.Extension
+{
+    public static class CameraExtension
+    {
+        public static Bounds OrthographicBounds( Camera camera)
+        {
+            float cameraHeight = camera.orthographicSize * 2;
+            Bounds bounds = new Bounds(
+                camera.transform.position,
+                new Vector3(cameraHeight * camera.aspect, cameraHeight, 0));
+            return bounds;
+        }
+    }
+}
