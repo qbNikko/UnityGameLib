f030ffa4c61d2c931569b8330b28579b37d48914
zhulkov <qbNikko@gmail.com>
2025-08-17 15:52:43 +0400

    ForwardBulletEmitter Gizmos

--- Runtime/Component/Bullet/ForwardBulletEmitter.cs
+++ Runtime/Component/Bullet/ForwardBulletEmitter.cs
@@ -1,10 +1,28 @@
-﻿namespace UnityGameLib.Component.Bullet
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace UnityGameLib.Component.Bullet
 {
     public class ForwardBulletEmitter : BulletEmitter
     {
         protected override void ConfigBullet(BulletComponent bullet)
         {
             bullet.Direction = transform.up;
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+#if UNITY_EDITOR
+            Gizmos.color = Color.red;
+            
+            Gizmos.DrawLine(
+                Vector3.zero,
+                transform.up
+            );
+            Gizmos.DrawWireSphere(Vector3.zero, 0.05f);
+            Gizmos.color = Color.white;
+#endif
         }
     }
 }
