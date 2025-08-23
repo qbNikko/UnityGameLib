c7395892b6be24e7d158c874c1438b3c8fe9813f
zhulkov <qbNikko@gmail.com>
2025-08-23 23:03:01 +0400

    FSM

---
+++ Runtime/FSM/IState.cs
@@ -0,0 +1,17 @@
+﻿namespace UnityGameLib.FSM
+{
+    public interface IState
+    {
+        public string Name();
+        public StateMachine StateMachine { get; internal set; }
+
+        public void Enter() {}
+        
+        public void Exit() {}
+        
+        public void Update() { }
+        
+        public void FixedUpdate() {}
+        void HandleEvent(string eventName) {}
+    }
+}
---
+++ Runtime/FSM/StateMachine.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityGameLib.Collection;
+using UnityGameLib.Component.Utils;
+
+namespace UnityGameLib.FSM
+{
+    public class StateMachine
+    {
+        private Dictionary<string, IState> _statesDictionary;
+        private Dictionary<Type, IState> _statesTypeDictionary;
+        private DisposeContainer  _disposableContainer;
+        private IState _currentState;
+
+        public StateMachine()
+        {
+            _disposableContainer.AddDisposable(CollectionPool.GetDictionary(out _statesTypeDictionary));
+            _disposableContainer.AddDisposable(CollectionPool.GetDictionary(out _statesTypeDictionary));
+        }
+
+        public StateMachine RegisterState(IState state)
+        {
+            _statesDictionary.Add(state.Name(), state);
+            _statesTypeDictionary.Add(state.GetType(), state);
+            state.StateMachine = this;
+            return this;
+        }
+        
+        public void SwitchState(Type state)
+        {
+            if (_statesTypeDictionary.TryGetValue(state, out var newState))
+            {
+                _currentState?.Exit();
+                _currentState = newState;
+                _currentState.Enter();
+            }
+        }
+        
+        public void SwitchState(string state)
+        {
+            if (_statesDictionary.TryGetValue(state, out var newState))
+            {
+                _currentState?.Exit();
+                _currentState = newState;
+                _currentState.Enter();
+            }
+        }
+
+        public void HandleEvent(string eventName)
+        {
+            _currentState?.HandleEvent(eventName);
+        }
+
+        public void Update()
+        {
+            _currentState?.Update();
+        }
+
+        public void FixedUpdate()
+        {
+            _currentState?.FixedUpdate();
+        }
+    }
+}
