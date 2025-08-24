ae4fe4323efb76bb8ef3b12769e59c571f5eea86
zhulkov <qbNikko@gmail.com>
2025-08-24 11:55:01 +0400

    Реализация FSM AnimationSubState

---
+++ Runtime/FSM/SubState.cs
@@ -0,0 +1,14 @@
+﻿namespace UnityGameLib.FSM
+{
+    public abstract class SubState : ISubState
+    {
+        private IState _state;
+        public bool Enabled { get; set; }
+
+        IState ISubState.State
+        {
+            get => _state;
+            set => _state = value;
+        }
+    }
+}
